617ac72bbc9e4eb6ed45c2053996af9892f9057d
Minh Do <mackcest@gmail.com>
2023-03-14 00:36:46 +0700

    add"

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import "../assets/style.css"
+function MainPage(props) {
+    return (
+        <div>
+            <div className="elementor-inner">
+                <div className="elementor-section-wrap">
+                    <section className="elementor-element elementor-element-ea4764f elementor-section-height-full elementor-section-items-top elementor-section-boxed elementor-section-height-default elementor-section elementor-top-section" data-id="ea4764f" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+                        <div className="elementor-container elementor-column-gap-default">
+                            <div className="elementor-row">
+                                <div className="elementor-element elementor-element-aa05b18 elementor-column elementor-col-100 elementor-top-column" data-id="aa05b18" data-element_type="column">
+                                    <div className="elementor-column-wrap elementor-element-populated">
+                                        <div className="elementor-widget-wrap">
+                                            <section className="elementor-element elementor-element-e66ce89 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section" data-id="e66ce89" data-element_type="section">
+                                                <div className="elementor-container elementor-column-gap-default">
+                                                    <div className="elementor-row">
+                                                        <div className="elementor-element elementor-element-bd7dcb9 elementor-column elementor-col-100 elementor-inner-column" data-id="bd7dcb9" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+                                                            <div className="elementor-column-wrap elementor-element-populated1">
+                                                                <div className="elementor-widget-wrap1">
+                                                                    <div className="elementor-element elementor-element-bf2d905 elementor-widget elementor-widget-image" data-id="bf2d905" data-element_type="widget" data-widget_type="image.default">
+                                                                        <div className="elementor-widget-container">
+                                                                            <div className="elementor-image">
+                                                                                <img width={117} height={117} src="https://simpleweb1.cdn.vccloud.vn/wp-content/uploads/2023/03/5023650.jpg" className="attachment-full size-full lazyloaded" alt="" data-ll-status="loaded" />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="elementor-element elementor-element-11349f9 elementor-widget elementor-widget-heading" data-id="11349f9" data-element_type="widget" data-widget_type="heading.default">
+                                                                        <div className="elementor-widget-container">
+                                                                            <h2 className="elementor-heading-title elementor-size-default">Sàn Giao Dịch Chứng Khoán</h2>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="elementor-element elementor-element-a7347ff elementor-widget elementor-widget-heading" data-id="a7347ff" data-element_type="widget" data-widget_type="heading.default">
+                                                                        <div className="elementor-widget-container">
+                                                                            <h2 className="elementor-heading-title-20 elementor-size-default">NỀN TẢNG GIAO DỊCH HÀNG ĐẦU</h2>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="elementor-element elementor-element-2237ba5 elementor-align-center elementor-widget elementor-widget-bdt-fancy-list" data-id="2237ba5" data-element_type="widget" data-widget_type="bdt-fancy-list.default">
+                                                                        <div className="elementor-widget-container">
+                                                                            <div className="bdt-fancy-list">
+                                                                                <ul className="bdt-list custom-list-group">
+                                                                                    <li>
+                                                                                        <a href="https://sgstockexchange.com/register?ref=DL10" target="_blank">
+                                                                                            <div className="bdt-flex flex-wrap">
+                                                                                                <div className="list-text ">
+                                                                                                    <h4 className="bdt-list-title"> TRANG CHỦ</h4>
+                                                                                                    <p className="list-text-des" />
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </a>
+                                                                                    </li>
+                                                                                    <li>
+                                                                                        <a href="https://sgstockexchange.com/register?ref=DL10" target="_blank">
+                                                                                            <div className="bdt-flex flex-wrap">
+                                                                                                <div className="list-text ">
+                                                                                                    <h4 className="bdt-list-title"> ĐĂNG KÝ NGAY</h4>
+                                                                                                    <p className="list-text-des" />
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </a>
+                                                                                    </li>
+                                                                                </ul>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="elementor-element elementor-element-34ec677 elementor-widget elementor-widget-heading" data-id="34ec677" data-element_type="widget" data-widget_type="heading.default">
+                                                                        <div className="elementor-widget-container">
+                                                                            <h2 className="elementor-heading-title elementor-size-default">
+                                                                                <a href="https://sgstockexchange.com/register?ref=DL10" target="_blank">NỀN TẢNG GIAO DỊCH ĐÁNG TIN CẬY</a>
+                                                                            </h2>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+MainPage.propTypes = {}
+
+export default MainPage
